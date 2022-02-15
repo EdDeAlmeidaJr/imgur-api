1410a4bfa015bdbc9ac50fa8e42734c0d4b37c4f
Ed de Almeida <edvaldoajunior@gmail.com>
2022-02-15 15:24:27 -0300

    Adding a .env file and dotenv module

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var authRouter = require('./routes/auth');
 
 var app = express();
